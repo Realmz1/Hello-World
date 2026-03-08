43726844a5572c1cda3e7c10f3cd60d9571dc077
dylanrhoton <rho20013@byui.edu>
2026-03-08 10:45:44 -0700

    started Module 1 assignment

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# Purpose: Intro assignment for CSE 310
+
+def greet_user():
+    print("------------------------------------------")
+    print("Hello, World!")
+    print("Welcome to the Target Price Trader dev hub.")
+    print("Let's build some high-performance C++ soon!")
+    print("------------------------------------------")
+
+if __name__ == "__main__":
+    greet_user()
